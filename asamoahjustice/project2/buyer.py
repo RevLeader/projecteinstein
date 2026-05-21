9a077a58c2ef61f021ad980d6e5748cbe0b40dfd
unknown <apostlekwadwo719@gmail.com>
2026-05-21 00:01:49 +0000

    added validators.py

--- asamoahjustice/project2/buyer.py
+++ asamoahjustice/project2/buyer.py
@@ -1,2 +1,11 @@
 from constant import SEPARATOR, DIVIDER
 
+#registring buyer
+def register_buyer():
+    details = ["name", "number", "location", "item", "budget"]
+    buyer_data = {} #initialising buyer data variable
+
+    for info in details:
+        while True:
+            value = input(f"Please enter your {info.capitalise()}")
+            
---
+++ asamoahjustice/project2/validators.py
@@ -0,0 +1,28 @@
+def validate_name(value):
+    #if the name is empty
+    if not value:
+        return False, "Please name cannot be empty"
+
+    #if name is not 2 or more words long
+    if len(value.split()) > 2:
+        return False, "Enter your full name"
+    
+    #return true if all passes
+    return True, None
+    
+    #validation for number input
+def validate_number(value):
+    #if the number is int
+    if not value.isdigit():
+        return False, "Number must be all digits(0-9)"
+    
+    #if the number starts with 0
+    if not value.startswith("0"):
+        return False, "Number must start with 0"
+    
+    #if the number is exactly 10character long
+    if len(value) != 10:
+        return False, "Number must be exactly 10numbers long" 
+    
+    return True, None
+    
